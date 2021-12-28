1e029ab63631fbcb6ad08e089fe85278961cb6f9
odajunpei <i.penju1022@gmail.com>
2021-12-28 07:05:00 +0000

    inquiry

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,2 +1,26 @@
 class InquiriesController < ApplicationController
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      InquiryMailer.received_email(@inquiry).deliver
+      redirect_to "/inquiries/#{@inquiry.name}/#{@inquiry.id}"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :title, :inquiry, :reply, :replyok, :id)
+  end
+
 end
