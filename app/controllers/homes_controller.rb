ad096c0a621b183a2272de7911dd265f41c6e2a2
odajunpei <i.penju1022@gmail.com>
2022-01-11 10:19:58 +0000

    profile

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,7 @@ class HomesController < ApplicationController
   def top
     @applications = Application.all
   end
+
+  def profile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'homes#top'
-
+  resources :applications, only: [:index, :create, :show]
+  get 'homes/profile' => 'homes#profile'
    namespace :admin do
     resources :applications, only: [:index, :create, :show, :edit, :update, :new]
   end
