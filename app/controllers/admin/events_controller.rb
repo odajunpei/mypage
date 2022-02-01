ca5dee3f39af944e8f199a98e1b00dd361b2a1cc
odajunpei <i.penju1022@gmail.com>
2022-02-01 15:24:12 +0000

    event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,9 +1,46 @@
 class Admin::EventsController < ApplicationController
 
   before_action :authenticate_admin!
-  
-  def create 
+
+  def new
+    @event = Event.new
   end
   
+  def index
+    @events = Event.all
+    
+
+  def create
+    @event = Event.new(eventparams)
+    if @event.save
+      redirect_to admin_applications_path
+    else
+      render :index
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
   
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(param[:id])
+    if @event.update(event_params)
+      redirect_to admin_application_path(@event)
+    else
+      render :edit
+    end
+  end
+
+    private
+  
+    def event_params
+      params.require(:event).permit(:name, :introduction, :date, :url, :giturl)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root to: 'applications#index'
   resources :applications, only: [:index, :show]
   resources :inquiries, only: [:new, :create], param: :name
+  resources :event, only: [:show]
   get 'inquiries/:name/:id' => 'inquiries#show'
   get 'homes/profile' => 'homes#profile'
    namespace :admin do
     resources :applications, only: [:index, :create, :show, :edit, :update, :new, :destroy]
     resources :inquiries, only: [:index, :edit, :update]
+    resources :events, only: [:new, :edit, :create, :update, :show, :index, :destroy]
   end
 end
